05b2b776c31ff61905f0fe9749054c0be2ddaa58
Lucas Rocha dos Santos <lucasrocha77706@gmail.com>
2024-02-08 18:42:59 -0300

    Add files via upload

---
+++ Nélio Alves/curso_programacao/src/main/java/com/mycompany/curso_programacao/Curso_programacao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curso_programacao;
+
+/**
+ *
+ * @author lucasrocha
+ */
+public class Curso_programacao {
+
+    public static void main(String[] args) {
+        String nome= "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        
+        System.out.printf("%s tem %d anos de idade e recebe mensalmente R$%.2f.", nome, idade, renda);
+    }
+}
---
+++ Semana 1/MeuProjeto/src/main/java/com/mycompany/meuprojeto/MeuProjeto.java
@@ -0,0 +1,9 @@
+package com.mycompany.meuprojeto;
+
+
+public class MeuProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo à UFABC!");
+    }
+}
